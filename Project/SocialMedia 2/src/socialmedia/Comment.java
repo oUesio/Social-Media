20c98f950a85eb247d1a1d5fa9d991665f0ec4fc
oUesio <116362669+oUesio@users.noreply.github.com>
2023-03-07 11:04:53 +0000

    Update Comment.java
    added comment and endorsement list and relating methods

--- Project/SocialMedia 2/src/socialmedia/Comment.java
+++ Project/SocialMedia 2/src/socialmedia/Comment.java
@@ -1,8 +1,12 @@
 
-//package socialmedia;
+package socialmedia;
+
+import java.util.ArrayList;
 
 public class Comment extends Post{
     private int postReferenceID;
+    private ArrayList<Integer> endorsementsList = new ArrayList<Integer>();
+    private ArrayList<Integer> commentsList = new ArrayList<Integer>(); 
 
     public Comment(int accountID, String message, int postReferenceID) {
         super(accountID, message);
@@ -11,6 +15,22 @@ public class Comment extends Post{
 
     public int getPostReferenceID(){
         return postReferenceID;
+    }
+
+    public void addEndorsementID(int endorsementID){ 
+        endorsementsList.add(Integer.valueOf(endorsementID));
+    }
+
+    public void addCommentID(int commentID){ 
+        commentsList.add(Integer.valueOf(commentID));
+    }
+
+    public ArrayList<Integer> getEndorsementsList(){
+        return endorsementsList;
+    }
+
+    public ArrayList<Integer> getCommentsList(){
+        return commentsList;
     }
     
     //what is different to the endorsement?
