3978d9cdc055a789985e60699ee7a444de7ef845
oUesio <116362669+oUesio@users.noreply.github.com>
2023-03-07 13:58:31 +0000

    Update Comment.java
    revert changes in comment

--- Project/SocialMedia 2/src/socialmedia/Comment.java
+++ Project/SocialMedia 2/src/socialmedia/Comment.java
@@ -1,11 +1,7 @@
 package socialmedia;
 
-import java.util.ArrayList;
-
 public class Comment extends Post{
     private int postReferenceID;
-    private ArrayList<Integer> endorsementsList = new ArrayList<Integer>();
-    private ArrayList<Integer> commentsList = new ArrayList<Integer>(); 
 
     public Comment(int accountID, String message, int postReferenceID) {
         super(accountID, message);
@@ -14,39 +10,5 @@ public class Comment extends Post{
 
     public int getPostReferenceID(){
         return postReferenceID;
-    }
-
-    public void addEndorsementID(int endorsementID){ 
-        endorsementsList.add(Integer.valueOf(endorsementID));
-    }
-
-    public void addCommentID(int commentID){ 
-        commentsList.add(Integer.valueOf(commentID));
-    }
-
-    public ArrayList<Integer> getEndorsementsList(){
-        return endorsementsList;
-    }
-
-    public ArrayList<Integer> getCommentsList(){
-        return commentsList;
-    }
-    
-    public void removeEndorsementID(int endorsementID){
-        for (int pos = 0; pos < endorsementsList.size(); pos++) {
-            if (endorsementsList.get(pos) == endorsementID) {
-                endorsementsList.remove(pos);
-                break;
-            }
-        }
-    }
-
-    public void removeCommentID(int commentID){
-        for (int pos = 0; pos < commentsList.size(); pos++) {
-            if (commentsList.get(pos) == commentID) {
-                commentsList.remove(pos);
-                break;
-            }
-        }
     }
 }
