2bfa046ad5549494bc48e3d7ff49c9d824e94189
oUesio <116362669+oUesio@users.noreply.github.com>
2023-03-12 14:09:51 +0000

    Update BadSocialMedia.java
    added array list to store placeholder posts

--- Project/SocialMedia 2/src/socialmedia/BadSocialMedia.java
+++ Project/SocialMedia 2/src/socialmedia/BadSocialMedia.java
@@ -14,6 +14,7 @@ import javax.print.attribute.standard.NumberOfDocuments;
  */
 public class BadSocialMedia implements SocialMediaPlatform {
 	private ArrayList<Account> accountsList = new ArrayList<Account>();
+	private ArrayList<DeletedPost> deletedPostsList = new ArrayList<DeletedPost>();
 
 	@Override
 	public int createAccount(String handle) throws IllegalHandleException, InvalidHandleException {
