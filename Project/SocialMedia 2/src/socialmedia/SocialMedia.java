7decb927632452a60f19489441c9bb9dbb541242
oUesio <116362669+oUesio@users.noreply.github.com>
2023-03-26 19:04:54 +0100

    imports needed for platform methods

--- Project/SocialMedia 2/src/socialmedia/SocialMedia.java
+++ Project/SocialMedia 2/src/socialmedia/SocialMedia.java
@@ -2,6 +2,12 @@ package socialmedia;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.Dictionary;
+import java.util.Hashtable;
 
 public class SocialMedia implements SocialMediaPlatform {
 	private ArrayList<Account> accountsList = new ArrayList<Account>();
