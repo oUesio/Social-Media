46d3e2ef6a790ef5975d1c00c3c76d42dbd0d818
oUesio <116362669+oUesio@users.noreply.github.com>
2023-03-30 12:02:14 +0100

    serial

--- Project/SocialMedia 2/src/socialmedia/DeletedComment.java
+++ Project/SocialMedia 2/src/socialmedia/DeletedComment.java
@@ -3,6 +3,8 @@ package socialmedia;
 import java.util.ArrayList;
 
 public class DeletedComment extends DeletedPost{
+    private static final long serialVersionUID = 6L;
+    
     private int postReferenceID;
 
     public DeletedComment(int postID, ArrayList<Integer> commentsList, int postReferenceID) {
