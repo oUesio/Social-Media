21cf6936b95a075c875dfbcc58a8be19a7a8ed12
oUesio <116362669+oUesio@users.noreply.github.com>
2023-03-30 11:57:36 +0100

    serial

--- Project/SocialMedia 2/src/socialmedia/Account.java
+++ Project/SocialMedia 2/src/socialmedia/Account.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.util.ArrayList;
 
 public class Account implements Serializable {
+    private static final long serialVersionUID = 1L;
     
     private static int numberOfAccounts = 0;
     private int accountID;
