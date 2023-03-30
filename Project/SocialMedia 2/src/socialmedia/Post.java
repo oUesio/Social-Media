7d939f50f97f8dea6b96bf5c54ae4ab3cebdcb81
oUesio <116362669+oUesio@users.noreply.github.com>
2023-03-30 11:58:05 +0100

    serial

--- Project/SocialMedia 2/src/socialmedia/Post.java
+++ Project/SocialMedia 2/src/socialmedia/Post.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 import java.util.ArrayList;
 
 public class Post implements Serializable {
+    private static final long serialVersionUID = 2L;
+    
     private int postID;
     protected static int numberOfPosts = 0;
     private int accountID;
